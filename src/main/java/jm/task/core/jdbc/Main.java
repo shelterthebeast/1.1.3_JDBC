5a2f4f7cf5500752c3cc951112a5656faf40e922
shelterthebeast <sinerfoil@mail.ru>
2025-05-05 22:59:36 +0700

    Задача выполнена

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,35 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static final UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("John", "Doe", (byte) 30);
+        userService.saveUser("Jane", "Smith", (byte) 25);
+        userService.saveUser("Mike", "Johnson", (byte) 40);
+        userService.saveUser("Emily", "Davis", (byte) 35);
+
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
     }
 }
